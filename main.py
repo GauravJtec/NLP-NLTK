a0b7a763ad01d66c436b96d8c0587c2c7975babb
GauravJtec <gauravtechj@gmail.com>
2023-02-02 11:38:09 +0530

    commit

---
+++ main.py
@@ -0,0 +1,25 @@
+text=open('read.txt',encoding='utf-8').read()
+lower_case=text.lower()
+import string
+clean=lower_case.translate(str.maketrans('','',string.punctuation))
+tokenised=clean.split()
+print(string.punctuation)
+print(lower_case)
+print(clean)
+print(tokenised)
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+final=[]
+for i in tokenised:
+    if i not in stop_words:
+        final.append(i)
+print (final)                      
